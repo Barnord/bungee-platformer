c220e9382ea0d8751d9c0aaa55f31d6b1fbf61f2
Ben Arno <bdarno92@gmail.com>
2021-09-10 08:25:14 -0700

    hookshot functional

--- bungee-platformer/Assets/Scripts/PlayerMovement.cs
+++ bungee-platformer/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Transform debugHitPointTransform;
+
     public CharacterController controller;
+    public Camera camera;
     public float speed = 12f;
     public float gravity = -9.81f;
     public Transform groundCheck;
@@ -16,8 +19,32 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    private Vector3 hookshotPosition;
+    private Vector3 PlayerMomentum;
+    private State state = State.Normal;
+    private enum State
+    {
+        Normal,
+        HookshotFlyingPlayer
+    }
+
     // Update is called once per frame
     void Update()
+    {
+        switch (state)
+        {
+            default:
+            case State.Normal:
+                HandleMovement();
+                HandleHookshotStart();
+                break;
+            case State.HookshotFlyingPlayer:
+                HandleHookshotMovement();
+                break;
+        }
+    }
+
+    private void HandleMovement()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -41,5 +68,58 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void HandleHookshotStart()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(controller.transform.position, controller.transform.forward, out RaycastHit raycastHit)) {
+                debugHitPointTransform.position = raycastHit.point;
+                hookshotPosition = raycastHit.point;
+                state = State.HookshotFlyingPlayer;
+            }
+        }
+    }
+
+    private void ResetGravityEffect()
+    {
+        velocity = Vector3.zero;
+    }
+
+    private void HandleHookshotMovement()
+    {
+        Vector3 hookshotDir = (hookshotPosition - transform.position).normalized;
+
+        float hookshotSpeedMin = 10f;
+        float hookshotSpeedMax = 40f;
+        float hookshotSpeed = Mathf.Clamp(Vector3.Distance(transform.position, hookshotPosition), hookshotSpeedMin, hookshotSpeedMax);
+        float hookshotSpeedMultiplier = 2f;
+
+        controller.Move(hookshotDir * hookshotSpeed * hookshotSpeedMultiplier * Time.deltaTime);
+
+        float reachedHookshotPositionDistance = 1.5f;
+
+        if (Vector3.Distance(transform.position, hookshotPosition) < reachedHookshotPositionDistance)
+        {
+            state = State.Normal;
+            ResetGravityEffect();
+        }
+
+        if (TestInputDownHookshot())
+        {
+            state = State.Normal;
+            ResetGravityEffect();
+        }
+    }
+
+    private bool TestInputDownHookshot()
+    {
+        return Input.GetKeyDown(KeyCode.E);
+    }
+
+    private bool TestInputJump()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
     }
 }
